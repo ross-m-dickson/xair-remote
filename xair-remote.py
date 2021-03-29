e915e686fa139eb827c3ace71cdcc797b3a320b2
Ross Dickson <ross.dickson@astc-design.com>
2021-03-28 20:42:02 -0400

    added debug command line options and rewrote help text to match new functionality.

--- xair-remote.py
+++ xair-remote.py
@@ -6,9 +6,29 @@ from lib.xair import XAirClient, find_mixer
 from lib.mixerstate import MixerState
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = 'Remote control X-Air mixers with a midi controller')
+    parser = argparse.ArgumentParser(description = """
+    Remote control X-Air mixers with an X-Touch midi controller.
+
+    The X-Touch is setup so that the encoders are configured as faders on any of
+    10 banks with the first row of button configured as mutes for the selected
+    bank. Banks are selected with the buttons in the lower row and the layer
+    buttons. Most banks have two layers, the second access by pressing the
+    button a second time, causing the button to blink. The fader is not used.
+    Pressing an encoder returns the level to unity gain, not used for mic pre.
+    """,
+    epilog= """
+    Bank 1-6 - Aux Bus 1-6 levels for Channels 1-8 and 9-16.
+    Bank 7 - Mic PreAmp levels for Channels 1-8 and 9-16.
+    Bank 8 - TBD
+    Layer A - Main LR Mix levels of Channels 1-8 and 9-16 on second layer.
+    Layer B - Master/Mix levels for Main/LR Bus, USB IN, Aux Bus 1-6.
+    """,
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+    # the fader is not used as I don't have a good global use and a bank
+    # specific use doesn't work well with a non moving fader
     parser.add_argument('xair_address', help = 'ip address of your X-Air mixer (optional)', nargs = '?')
     parser.add_argument('-m', '--monitor', help='monitor X-Touch connection and exit when disconnected', action="store_true")
+    parser.add_argument('-d', '--debug', help='enable debug output', action="store_true")
     args = parser.parse_args()
 
     if args.xair_address is None:
@@ -26,6 +46,9 @@ if __name__ == '__main__':
     state.xair_client = xair
     xair.validate_connection()
 
+    state.debug = args.debug
+    midi.debug = args.debug
+
     if args.monitor:
         print('Monitoring X-Touch connection enabled')
         monitor = threading.Thread(target = midi.monitor_ports)
@@ -33,6 +56,7 @@ if __name__ == '__main__':
         monitor.start()
     
     state.read_initial_state()
-    
+    midi.activate_bus(8)                    # set chanel level as initial bus
+
     # now refresh /xremote command while running
     xair.refresh_connection()
