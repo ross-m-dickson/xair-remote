58333ba16c4f2438c5104bf015cebdd62dbd4683
Ross Dickson <ross.m.dickson@gmail.com>
2021-04-02 20:30:51 -0400

    Added alternate main functions that adds a menu and will eventually add additional functions.

---
+++ lib/screen.py
@@ -0,0 +1,133 @@
+"""
+This module holds the state of the touch screen and buttons
+"""
+import os
+import pygame
+#import subprocess
+
+from gpiozero import Button
+
+
+class GPIO_Button:
+    """Stores a GPIO button, its state, and the event callback function."""
+    enable = 1
+
+    def __init__(self, number, pos):
+        self.button_GPIO = Button(number)
+        self.event_obj = pygame.event.Event(pygame.USEREVENT + 1 + pos,
+                                            message="Button %d" % pos)
+        self.button_GPIO.when_pressed = self.button_callback
+
+    def button_callback(self):
+        """Callback for when button event triggers."""
+        if self.enable == 0:
+            self.enable = 1
+        else:
+            self.enable = 0
+
+class Screen:
+    """
+    This stores the state associated with drawing elements on the screen. It
+    also holds the function to draw the screen
+    """
+
+    # pointers to other modules
+    xair_thread = None
+    osc_thread = None
+    midi_thread = None
+
+    # define names for numbers
+    size = width, height = 320, 240
+    white = 255, 255, 255
+    yellow = 254, 255, 25
+    black = 0, 0, 0
+    red = 255, 0, 0
+    yellow = 255, 255, 0
+    green = 0, 255, 0
+    blue = 26, 0, 255
+
+    box_left = 10
+    box_width = 210
+    margin = box_left + 4
+    title_center = (box_left + box_width)/2
+    button_left = box_left * 1.5 + box_width
+    button_width = 320 - button_left - box_left/2
+
+    #define strings
+    names = ("Bass", "Guitar", "Keys", "Sax", "Ross", "Raluca", "Butch", "Ors",
+             "Itai", "Guest", "Guest", "Guest", "Kick", "Snare", "Toms", "Hat")
+    words = []
+    numbers = []
+    mic = ("1/4", "1/4", "1/4", "pga", "cm", "58", "58", "ors",
+           "any", "", "", "", "pga", "58", "akg", "akg")
+    mics = []
+    button_nm = ("Remote", "Record", "Screen Off", "Setup",
+                 "WiFi", "Auto Level", "Power Off", "Return",
+                 "Confirm", "Confirm", "Return", "Return")
+    button_img = []
+
+    def __init__(self):
+        # initialize the pygame infrastructure
+        os.environ["SDL_FBDEV"] = "/dev/fb1"
+        pygame.init()
+        pygame.mouse.set_visible(False)
+
+        # define pygame values
+        self.screen = pygame.display.set_mode((320, 240))
+        # define font sizes
+        font = pygame.font.Font(None, 24)
+        smfont = pygame.font.Font(None, 19)
+        title_font = pygame.font.Font(None, 34)
+
+        # initialize GPIO buttons
+        self.gpio_button = (GPIO_Button(17, 0), GPIO_Button(22, 1),
+                            GPIO_Button(23, 2), GPIO_Button(27, 3))
+
+        #convert strings to surfaces
+        self.title = title_font.render("X Air Control", 1, (self.blue))
+        self.sub_title = font.render("Channel Setup", 1, (self.blue))
+        for x in range(16):
+            self.words.append(pygame.transform.rotate(font.render(self.names[x], 1, (self.red)), 315))
+            self.numbers.append(font.render("%02d" % (x+1), 1, (self.red)))
+            self.mics.append(smfont.render(self.mic[x], 1, (self.red)))
+        for x in range(12):
+            self.button_img.append(font.render(self.button_nm[x], 1, (self.blue)))
+
+        self.title_w, self.title_h = self.title.get_size()
+        self.subtitle_w, self.subtitle_h = self.sub_title.get_size()
+        self.num_w, self.num_h = self.numbers[7].get_size()
+
+
+    def screen_loop(self):
+        """Update the screen after an event."""
+        while 1:
+#            for event in pygame.event.get():
+#                if event.type == pygame.QUIT: sys.exit()
+
+            # set the background
+            self.screen.fill(self.black)
+            pygame.draw.rect(self.screen, self.blue, (0, 0, 320, 240), 3) # outer border
+            # draw titles
+            self.screen.blit(self.title, (self.title_center-(self.title_w/2), 10))
+            self.screen.blit(self.sub_title, (self.title_center-(self.subtitle_w/2), 40)) #  (40,40))
+            # draw channel names
+            pygame.draw.rect(self.screen, self.red, (self.box_left, 68, self.box_width, 170), 1)
+            for x in range(8):
+                self.screen.blit(self.numbers[x], (self.margin   + (x * self.box_width/8), 70))
+                self.screen.blit(self.words[x], (self.box_left + (x * self.box_width/8), 70+self.num_h))
+                self.screen.blit(self.mics[x], (self.box_left + (x * self.box_width/8), 135))
+                self.screen.blit(self.numbers[x+8], (self.margin   + (x * self.box_width/8), 155))
+                self.screen.blit(self.words[x+8], (self.box_left + (x * self.box_width/8), 155+self.num_h))
+                self.screen.blit(self.mics[x+8], (self.box_left + (x * self.box_width/8), 220))
+            for x in range(4):
+                pygame.draw.rect(self.screen, self.yellow,
+                                 (self.button_left, 5+(x*60), self.button_width, 50),
+                                 self.gpio_button[x].enable)
+                self.screen.blit(self.button_img[x], (self.button_left + 4, 30 + (x*60)))
+
+            pygame.display.flip()
+
+#starts the xair_remote
+#the xair_recorder
+#the WiFi as a server
+#The fourth/bottom button opens a sub menu
---
+++ xair_control.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+import argparse
+import threading
+import xair_remote
+import os
+from lib.screen import Screen
+
+from time import sleep
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = """
+    Configure a Raspberry PI to record 18 track audio from and control X-Air
+    mixers with an X-Touch midi controller. This script assumes that the PI has
+    a PiFIT or similar hat that provides a framebuffer and has buttons at GPIO
+    17, 22, 23 and 27. Starting the app puts up a gui that is controlled by the
+    buttons. Currently any touch screen capabilities are disabled.
+
+    This descriptions assumes the PI is rotates with the HDMI and power ports at
+    the top, the buttons on the right and Ethernet/USB ports on the left.
+
+    The top/first button starts the xair_remote script, the second button starts
+    the xair_recorder, the third button configures the WiFi as a server and
+    enables a DHCP server on the wired Ethernet so that a connected XAir mixer
+    doesn't need a static IP address. The fourth/bottom button opens a sub menu
+    that includes an option to shut down the PI.
+
+    The X-Touch is setup so that the encoders are configured as faders on any of
+    10 banks with the first row of button configured as mutes for the selected
+    bank. Banks are selected with the buttons in the lower row and the layer
+    buttons. Most banks have two layers, the second access by pressing the
+    button a second time, causing the button to blink. The fader is not used.
+    Pressing an encoder returns the level to unity gain, not used for mic pre.
+    """,
+    epilog= """
+    Bank 1-6 - Aux Bus 1-6 levels for Channels 1-8 and 9-16.
+    Bank 7 - Aux Bus 7 aka FX 1 for Channels 1-8 and 9-16.
+    Bank 8 - Mic PreAmp levels for Channels 1-8 and 9-16.
+    Layer A - Main LR Mix levels of Channels 1-8 and 9-16 on second layer.
+    Layer B - Aux Bus 1-6 output levels, USB IN Gain, Main/LR Bus output level.
+    """,
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+    # the fader is not used as I don't have a good global use and a bank
+    # specific use doesn't work well with a non moving fader
+    parser.add_argument('xair_address', help = 'ip address of your X-Air mixer (optional)', nargs = '?')
+    parser.add_argument('-m', '--monitor', help='monitor X-Touch connection and exit when disconnected', action="store_true")
+    parser.add_argument('-d', '--debug', help='enable debug output', action="store_true")
+    args = parser.parse_args()
+
+    remote = xair_remote.XAirRemote(args.xair_address, args.monitor, args.debug)
+    xair_thread = threading.Thread(target=remote.xair.refresh_connection)
+    osc_thread = xair_remote.XAirClient.worker
+    midi_thread = xair_remote.MidiController.worker
+    screen_obj = Screen()
+    screen_obj.screen_loop()
+
+
+
+while True:
+    if button17.is_pressed:
+        print("Pressed 17")
+    if button22.is_pressed:
+        print("pressed 22")
+    if button23.is_pressed:
+        print("pressed 23")
+    if button27.is_pressed:
+        print("pressed 27")
+    sleep(.5)
+
+
+# 23, #22, #21, and #18 t
