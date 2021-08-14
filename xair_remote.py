671cf36bb05d44e3f467952ca68e9d5458d5990f
Ross Dickson <ross.dickson@astc-design.com>
2021-08-14 12:04:19 -0400

    updated text based on new fader assignment and renamed variables for clarity

--- xair_remote.py
+++ xair_remote.py
@@ -11,8 +11,9 @@ if __name__ == '__main__':
     10 banks with the first row of button configured as mutes for the selected
     bank. Banks are selected with the buttons in the lower row and the layer
     buttons. Most banks have two layers, the second access by pressing the
-    button a second time, causing the button to blink. The fader is not used.
-    Pressing an encoder returns the level to unity gain, not used for mic pre.
+    button a second time, causing the button to blink. The fader is used as a
+    master exit control. Pressing an encoder returns the level to unity gain,
+    not used for mic pre.
     """,
                                      epilog="""
     Bank 1-6 - Aux Bus 1-6 levels for Channels 1-8 and 9-16.
@@ -22,8 +23,6 @@ if __name__ == '__main__':
     Layer B - Aux Bus 1-6 output levels, USB IN Gain, Main/LR Bus output level.
     """,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
-    # the fader is not used as I don't have a good global use and a bank
-    # specific use doesn't work well with a non moving fader
     PARSER.add_argument('xair_address', help='ip address of your X-Air mixer (optional)', nargs='?')
     PARSER.add_argument('-m', '--monitor',
                         help='monitor X-Touch connection and exit when disconnected',
@@ -35,7 +34,7 @@ if __name__ == '__main__':
     PARSER.add_argument('-a', '--mac', help="use alternate mapping for mac", action="store_true")
     ARGS = PARSER.parse_args()
 
-    REMOTE = MixerState(ARGS)
-    if REMOTE.initialize_state():
+    STATE = MixerState(ARGS)
+    if STATE.initialize_state():
         # now start polling refresh /xremote command while running
-        REMOTE.xair_client.refresh_connection()
+        STATE.xair_client.refresh_connection()
